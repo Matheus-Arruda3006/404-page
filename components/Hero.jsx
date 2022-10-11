19b772377d9e0936d7180256e7e7377c1b7f1640
Matheus de Arruda <matheusarrudaf@gmail.com>
2022-10-11 17:02:32 -0300

    import link

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BgTech from '../src/assets/logotech.png';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 export default function Hero() {
@@ -9,8 +10,9 @@ export default function Hero() {
         {/* Overlay */}
         <div className='flex flex-col ml-10'>
             <h1>EM BREVE UM NOVO SITE</h1>
-        <Image src={BgTech} alt='logo-tech' width={300} height={300}/>
+        <Image src={BgTech} alt='logo-tech' width={400} height={500}/>
         <div>
+          <ul></ul>
           <p>comercial@flockmarketing.com.br</p>
           <p>@flockmkt</p>
           <p>55 65 9 9223-5891</p>
