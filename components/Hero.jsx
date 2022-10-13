16128b902a4dfef4b1cb7aa1c7f79b0e72d8ded7
Matheus-Arruda3006 <matheusarrudaf@gmail.com>
2022-10-12 21:18:54 -0300

    add-icons

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import BgTech from '../src/assets/logotech.png';
 import Image from 'next/image';
 import Link from 'next/link';
-import {FaFacebookF} from 'react-icons/fa';
+import {AiFillFacebook} from 'react-icons/ai';
 import { FaInstagram } from 'react-icons/fa';
 import {TbBrandGmail} from 'react-icons/tb';
 
@@ -13,11 +13,27 @@ export default function Hero() {
         {/* Overlay */}
         <div className='flex flex-col ml-10'>
             <h1>EM BREVE UM NOVO SITE</h1>
-        <Image src={BgTech} alt='logo-tech' width={300} height={300}/>
-        <div>
-          <p>comercial@flockmarketing.com.br</p>
-          <p>@flockmkt</p>
-          <p>55 65 9 9223-5891</p>
+        <Image src={BgTech} alt='logo-tech' width={400} height={500}/>
+        <div className='flex flex-col items-center'>
+            <div>
+                <Link href="https://web.facebook.com/flockmarketingeventos/?_rdc=10&_rdr" >
+                  <div className='flex flex-start'>
+                    <AiFillFacebook className='custom-icon1'/><a>Flock Marketing e Eventos</a>
+                  </div>
+                </Link>
+            </div>
+            <div>
+                <Link href="https://www.instagram.com/flockmkt/" >
+                  <div className='flex flex-start'>
+                    <FaInstagram className='custom-icon2'/><a>@flockmkt</a>
+                  </div>
+                </Link>
+            </div>
+            <div>
+                  <div className='flex flex-start'>
+                    <TbBrandGmail className='custom-icon'/><p>comercial@flockmarketing.com.br</p>
+                  </div>
+            </div>
         </div>
         </div>
     </div>
